bb58ee3c46d2bb0dea3e6840b549f6a130582810
Basel Korj <75984172+baselkorj@users.noreply.github.com>
2022-05-23 14:52:05 +0400

    overhaul

--- bootloader_patch.py
+++ bootloader_patch.py
@@ -1,22 +1,63 @@
-import os
-Import("env", "projenv")
+import sys
+import uuid
+Import("env")
 
 
 def after_build(source, target, env):
     bootloader = open("bootloader.hex")
     bootloader_contents = bootloader.readlines()
 
-    firmware = open(".pio/build/AVR_ISP_w_Bootloader/firmware.hex")
+    firmware = open(".pio/build/uno/firmware.hex")
     firmware_contents = firmware.readlines()
 
-    bootloader_contents.pop(0)
-    bootloader_contents.pop(len(bootloader_contents) - 1)
+    if (len(firmware_contents) > 2015):
+        sys.exit("Flash Memory is Full, No Space Left for UUID and Bootloader!")
+
+    firmware_contents.insert(0, ":020000040000FA\n")
+
+    firmware_contents.pop(len(firmware_contents) - 1)
+
+    print("\nMerging Bootloader...")
 
     for i in bootloader_contents:
-        firmware_contents.insert(len(firmware_contents) - 1, i)
+        firmware_contents.append(i)
 
-    firmware = open(".pio/build/AVR_ISP_w_Bootloader/firmware.hex", "w")
+    firmware = open(".pio/build/uno/firmware.hex", "w")
     firmware.writelines(firmware_contents)
 
+    print("Bootloader Merged.\n")
 
+
+def before_upload(source, target, env):
+    print("\nGenerating UUID & Calculating Checksums...")
+
+    uuidOne = uuid.uuid1()
+
+    firmware = open(".pio/build/uno/firmware.hex")
+    firmware_contents = firmware.readlines()
+
+    record_block = "107DF000" + uuidOne.hex
+
+    record_block_bytes = []
+
+    y = 0
+
+    while y < len(record_block):
+        record_block_bytes.append(int(record_block[y:y+2], 16))
+        y += 2
+
+    checksum = (~sum(record_block_bytes) + 1) & 0xFF
+
+    full_record_block = (":" + record_block + "%0.2X" %
+                         checksum + "\n").upper()
+
+    firmware_contents.pop(len(firmware_contents) - 35)
+    firmware_contents.insert(len(firmware_contents) - 34, full_record_block)
+
+    firmware = open(".pio/build/uno/firmware.hex", "w")
+    firmware.writelines(firmware_contents)
+    print("Success! Your Board ID is " + str(uuidOne))
+
+
+env.AddPreAction("upload", before_upload)
 env.AddPostAction("buildprog", after_build)
