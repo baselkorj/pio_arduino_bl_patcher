6cb48ad60bda49f780452fb885e228844addccb8
Basel Korj <75984172+baselkorj@users.noreply.github.com>
2022-05-29 11:26:29 +0400

    odoo integration and id write to firmware

---
+++ scripts/odoo/owrapper.py
@@ -0,0 +1,60 @@
+import odoo.oclient as client
+import os
+from getpass import getpass
+from datetime import datetime
+
+_url = 'odoodev.sulaimanhabsi.com'
+_db = 'aphcarios'
+_user = 'EMAIL'
+_pwd = "PASSWORD"
+
+_odoo = None
+
+
+def setCredentials(server='odoodev.sulaimanhabsi.com', user=_user, password=_pwd):
+    global _user, _pwd, _url
+    _user = user
+    _url = server
+    _pwd = password
+
+
+def signIn():
+    global _odoo
+    global _pwd
+    _odoo = client.OdooClient(host=_url,
+                              dbname=_db, protocol='xmlrpcs', port=443)
+
+    if _odoo:
+        try:
+            if os.path.exists('password.txt'):
+                # print('password file exists')
+                f = open('password.txt', 'r')
+                _pwd = f.read()
+            elif _pwd != '':
+                uid = _odoo.Authenticate(_user, _pwd)
+            else:
+                _pwd = getpass(prompt='Password: ', stream=None)
+                # print(p)
+                uid = _odoo.Authenticate(_user, _pwd)
+            return uid
+        except:
+            return False
+
+    else:
+        return False
+
+
+def getProductLots(productName):
+    products = _odoo.SearchRead('stock.production.lot', [
+                                ('product_id', '=', productName)], ['name'])
+    return products
+
+
+def createProductLot(name, product_id, firmware):
+    newProductLot = _odoo.Create('stock.production.lot', {
+        'name': name,
+        'product_id': product_id,
+        'company_id': 1,
+        'note': 'PCB Flash Date: ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ' | Firmware Version: ' + firmware
+    })
+    return newProductLot
